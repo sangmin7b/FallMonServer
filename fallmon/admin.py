f9a1456695c7ab30bee722d94a69f67a0172d612
sangmin7b <sangmin7b@gmail.com>
2023-12-06 02:54:41 +0900

    Add: admin page

--- fallmon/admin.py
+++ fallmon/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, FallType, FallHistory
+
+admin.site.register(User)
+admin.site.register(FallType)
+admin.site.register(FallHistory)
