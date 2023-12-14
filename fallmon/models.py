e05c321406ebd3230187003387051d14e1bfc7ea
sangmin7b <sangmin7b@gmail.com>
2023-12-15 05:09:06 +0900

    Fix: fix admin view

--- fallmon/models.py
+++ fallmon/models.py
@@ -8,19 +8,38 @@ class User(models.Model):
     pw = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
+
+    # Meta information for django admin
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
 
 
 class FallType(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=256, default="")
 
+    def __str__(self):
+        return f"[{self.id} {self.name}]"
+    # Meta information for django admin
+
+    class Meta:
+        verbose_name = "FallType"
+        verbose_name_plural = "FallTypes"
 
 class FallHistory(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     fall_type = models.ForeignKey(FallType, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.id} [user: {self.user_id} {self.created_at} {self.fall_type.name}]"
+
+    class Meta:
+        verbose_name = "FallHistory"
+        verbose_name_plural = "FallHistories"
 
 
 
