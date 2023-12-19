dd2963b69ccbd73eded37b6e82aee9c0277205d6
sangmin7b <sangmin7b@gmail.com>
2023-12-19 23:00:21 +0900

    Fix: fix user id field

--- fallmon/models.py
+++ fallmon/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
+    id = models.CharField(primary_key=True, max_length=256)
     pw = models.CharField(max_length=256)
 
     def __str__(self):
